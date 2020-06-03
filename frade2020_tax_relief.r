4e5bffa9bad8dd7f766321339e650c74cc16fda5
Rafael <rafaelfrade@live.com>
2020-06-02 22:22:32 -0300

    Refactoring

--- frade2020_tax_relief.r
+++ frade2020_tax_relief.r
@@ -1,76 +1,29 @@
 setwd('/home/rafael/arquivos/mestrado/2_term/metrics_II/paper/dataset')
 require(data.table)
-source('../code/translateNCMtoCNAE.r')
+source('../code/frade2020taxRelief/translateNCMtoCNAE.r')
 
-estab2012 = read.csv('/home/rafael/arquivos/mestrado/2_term/metrics_II/paper/dataset/Estb2012.txt', 
-                     stringsAsFactors=FALSE, fileEncoding="latin1", sep = ';')
-estab2012 = setDT(estab2012) 
-
-length(estab2012[Ind.Simples == 1 & Qtd.Vínculos.Ativos == 0])
-nrow(estab2012[Ind.Simples == 1 & Ind.Atividade.Ano == 1 & Qtd.Vínculos.CLT > 50])
-nrow(estab2012[Ind.Simples == 1 & Ind.Atividade.Ano == 1])
-nrow(estab2012[Ind.Simples == 0 & Ind.Atividade.Ano == 1 & Qtd.Vínculos.CLT > 50])
-nrow(estab2012[Ind.Simples == 0 & Ind.Atividade.Ano == 1])
 cnaeList = as.integer(fromNCMtoCNAE())
 
-nrow(estab2012[Ind.Simples == 1 & CNAE.2.0.Classe %chin% cnaeList])
-nrow(estab2012[Ind.Simples == 0 & CNAE.2.0.Classe %chin% cnaeList])
+# after_treatmend = 1 if year = 2013, 0 otherwise
+write_filtered_file = function(table, year, after_treatmend) {
+  table = setDT(table) 
+  table$eligible = as.numeric(table$CNAE.2.0.Classe %in% cnaeList)
+  table = table[Ind.Atividade.Ano == 1, 
+            .(Ind.Simples, eligible, Qtd.Vínculos.CLT, CNAE.2.0.Classe)]
 
-estab2012$eligible = as.numeric(estab2012$CNAE.2.0.Classe %in% cnaeList)
+  table$year = after_treatmend
+  
+  name = paste("firms", year, ".csv", sep="")
+  write.table(table, name)  
+}
+table = 
+csv2010 = read.csv('ESTB2010.txt', stringsAsFactors=FALSE, fileEncoding="latin1", sep = ';')
+write_filtered_file(csv2010, 2010, 0)
 
+csv2011 = read.csv('ESTB2011.txt', stringsAsFactors=FALSE, fileEncoding="latin1", sep = ';')
+write_filtered_file(csv2011, 2011, 0)
 
+csv2012 = read.csv('Estb2012.txt', stringsAsFactors=FALSE, fileEncoding="latin1", sep = ';')
+write_filtered_file('', 2012, 0)
+write_filtered_file('ESTB2013.txt', 2013, 1)
 
-Qtd.Vínculos.CLT>100
-estab2012$
-Ind.Simples == 0/1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#library(devtools)
-#devtools::install_github("lucasmation/microdadosBrasil")
-#if linux, install libarchive-dev with apt-get
-#devtools::install_github('jimhester/archive') 
-#download_sourceData("RAIS", i = "2012")
-#unzip_all_7z_rar(root_path = '/home/rafael/2012')
-
-mean(estab2012[Ind.Simples == 0]$Qtd.Vínculos.Ativos)
-
-estab2012[, .(.N), by = .(Ind.Simples)]
-cltSimples = estab2012[Qtd.Vínculos.CLT>100, .(quantile(Qtd.Vínculos.CLT, probs = seq(from = 0.1, to = 1, by=0.1))), by = Ind.Simples]
-
-hist(estab2012[Qtd.Vínculos.CLT > 100 & Qtd.Vínculos.CLT < 1000]$Qtd.Vínculos.CLT)
-
-estab2012[Qtd.Vínculos.CLT > 100 & Qtd.Vínculos.CLT < 1000, bins(Qtd.Vínculos.CLT, target.bins = 10), by = Ind.Simples]
-
-estab2012[3, sum(Qtd.Vínculos.CLT), by=CNAE.2.0.Classe]
-
-cnaeList = fromNCMtoCNAE()
-
-
-
-
-
-
-
-d2012 = read_RAIS('vinculos', i = 2012, 
-                  root_path = '/home/rafael/2012', UF=c('RR'));
-
-rr12 = read.csv('/home/rafael/2012/RR2012/RR2012.txt', sep = ';', stringsAsFactors=FALSE, fileEncoding="latin1")
-
-rr12 = setDT(rr12)
-summary(rr12$Natureza.Jurídica)
-library('microdadosBrasil')
