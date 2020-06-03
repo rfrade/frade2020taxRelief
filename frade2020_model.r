2c6276291c864573415a286e4479a2a69371e180
Rafael <rafaelfrade@live.com>
2020-06-02 23:22:56 -0300

    Refactoring

--- frade2020_model.r
+++ frade2020_model.r
@@ -1,4 +1,11 @@
 setwd('/home/rafael/arquivos/mestrado/2_term/metrics_II/paper/dataset')
 require(data.table)
 
-dados2011 = fread
+dados2010 = fread('firms2010.csv')
+dados2011 = fread('firms2011.csv')
+dados2012 = fread('firms2012.csv')
+dados2013 = fread('firms2013.csv')
+
+dados2010[]
+
+ggplot2::
--- frade2020_tax_relief.r
+++ frade2020_tax_relief.r
@@ -1,4 +1,4 @@
-setwd('/home/rafael/arquivos/mestrado/2_term/metrics_II/paper/dataset')
+setwd('/home/rafael/arquivos/mestrado/2_term/metrics_II/paper/dataset/non-cleaned')
 require(data.table)
 source('../code/frade2020taxRelief/translateNCMtoCNAE.r')
 
@@ -8,7 +8,7 @@ cnaeList = as.integer(fromNCMtoCNAE())
 write_filtered_file = function(table, year, after_treatment) {
   table = setDT(table) 
   table$eligible = as.numeric(table$CNAE.2.0.Classe %in% cnaeList)
-  table = table[Ind.Atividade.Ano == 1, 
+  table = table[Ind.Atividade.Ano == 1 & Qtd.Vínculos.CLT > 10,
             .(Ind.Simples, eligible, Qtd.Vínculos.CLT, CNAE.2.0.Classe)]
 
   table$year = after_treatment
@@ -18,14 +18,21 @@ write_filtered_file = function(table, year, after_treatment) {
 }
 
 csv2010 = read.csv('ESTB2010.txt', stringsAsFactors=FALSE, fileEncoding="latin1", sep = ';')
+print("read 2010")
 write_filtered_file(csv2010, 2010, 0)
+rm(data = csv2010)
 
 csv2011 = read.csv('ESTB2011.txt', stringsAsFactors=FALSE, fileEncoding="latin1", sep = ';')
+print("read 2011")
 write_filtered_file(csv2011, 2011, 0)
+rm(data = csv2011)
 
 csv2012 = read.csv('Estb2012.txt', stringsAsFactors=FALSE, fileEncoding="latin1", sep = ';')
-write_filtered_file('', 2012, 0)
+print("read 2012")
+write_filtered_file(csv2012, 2012, 0)
+rm(data = csv2012)
 
 csv2013 = read.csv('ESTB2013.txt', stringsAsFactors=FALSE, fileEncoding="latin1", sep = ';')
+print("read 2013")
 write_filtered_file(csv2013, 2013, 1)
-
+rm(data = csv2013)
